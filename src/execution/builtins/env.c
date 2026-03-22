160bddeeb72aa09b5c35702c8ffaf8e10f6adf9c
Thomas Alexander Exenberger <texenber@c3r8p4.42vienna.com>
2026-03-22 13:52:47 +0100

    tried to implement signals but it didn't work so I just commented them out

--- src/execution/builtins/env.c
+++ src/execution/builtins/env.c
@@ -6,12 +6,13 @@
 /*   By: texenber <texenber@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/21 09:59:41 by texenber          #+#    #+#             */
-/*   Updated: 2026/03/17 12:44:19 by texenber         ###   ########.fr       */
+/*   Updated: 2026/03/22 11:07:57 by texenber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../inc/execution.h"
 
+// in the env don't worry about _= because bash has a list of special variables in env
 int	builtin_env(char **envp) // CHANGE: add t_shell *shell
 {
 	int i;
